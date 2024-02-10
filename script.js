7b1e77e16dc4be8f955c0f8868b2ad0120df0c90
Ivan Bezrukov <ivan906730@gmail.com>
2024-02-10 14:59:57 +0300

    Add task2

---
+++ script.js
@@ -0,0 +1,21 @@
+const money = prompt('Ваш месячный доход?', 1000000);
+const expenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Food, Travel');
+const amount = prompt('Во сколько обойдутся обязательные статьи расходов?', 100000);
+const deposit = confirm('Есть ли у вас вклад в банке?');
+
+const budgetMonth = money - amount;
+console.log(budgetMonth);
+
+const purpose = 10000000;
+console.log(Math.ceil(purpose / budgetMonth));
+
+const budgetDay = budgetMonth / 30;
+console.log(Math.floor(budgetDay));
+
+if (budgetDay > 60000) {
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 30000 && budgetDay <= 60000) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay >= 0 && budgetDay < 30000) {
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+} else { console.log('Что-то пошло не так'); }
